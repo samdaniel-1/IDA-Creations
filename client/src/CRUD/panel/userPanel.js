82b3df6161e4ce23c698c9c939219a9c87a2d18c
samdaniel-1 <81462039+samdaniel-1@users.noreply.github.com>
2024-06-20 17:29:00 +0530

    user panel created

---
+++ client/src/CRUD/panel/userPanel.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Userpanel(){
+    return(
+<>
+<Link to={'/'} className="btn btn-warning m-5">Userpanel</Link>
+</>
+    );
+}
+export default Userpanel
