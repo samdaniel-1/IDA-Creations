fdc0d76fe743f3e1a9cd2e4d103a8db0cd0c1469
samdaniel-1 <81462039+samdaniel-1@users.noreply.github.com>
2024-06-20 17:32:20 +0530

    Files added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Root from './Visit/root';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return(
+    <>
+    <div className='main-bg-color'>
+    <Root/>
+    </div>
+    </>
+  )
+}
+
+export default App;
